90a91f9f48fa88c8af5a09eb9bfa33e059c02e46
Otoolorin <volasupo5@gmail.com>
2025-02-13 12:31:12 +0100

    Set number of rows and columns

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const container = document.querySelector('.container');
 
 const square = document.createElement('div');
+const noOfRows = 16;
+const noOfCols = 16;
+
 square.setAttribute('class', 'square');
 
 container.appendChild(square);
