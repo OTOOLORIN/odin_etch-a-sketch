3a816a72cd3136140e7d99e429c2708c21bdc365
Otoolorin <volasupo5@gmail.com>
2025-02-13 13:21:53 +0100

    Wrap the grid created in a new function
    This function, displayGrid() takes two arguments:
    number ooooooooof rows and colllumns, and use that
    to create a grid.

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-const container = document.querySelector('.container');
-
-const square = document.createElement('div');
 const noOfRows = 16;
 const noOfCols = 16;
-const rowGaps = 5 * noOfRows;
-const colGaps = 10 * noOfCols;
 
-const totalSquares = noOfRows * noOfCols;
+function displayGrid(rows, cols){
+  const container = document.querySelector('.container');
 
-square.setAttribute('class', 'square');
+  const square = document.createElement('div');
+  const rowGaps = 5 * noOfRows;
+  const colGaps = 10 * noOfCols;
 
-container.appendChild(square);
+  const totalSquares = noOfRows * noOfCols;
 
-const squareWidth = square.offsetWidth;
-const squareHeight = square.offsetHeight;
-const containerWidth = squareWidth * noOfCols
-const containerHeight = squareHeight * noOfRows;
+  square.setAttribute('class', 'square');
 
-container.className = 'container';
+  container.appendChild(square);
 
-container.style.width =  `${containerWidth+colGaps}px`;
-container.style.height = `${containerHeight+rowGaps}px`;
+  const squareWidth = square.offsetWidth;
+  const squareHeight = square.offsetHeight;
+  const containerWidth = squareWidth * noOfCols
+  const containerHeight = squareHeight * noOfRows;
 
-let noOfSquares = 1;
-while (noOfSquares < totalSquares) {
-  const squareCopy = square.cloneNode(true);
-  container.appendChild(squareCopy);
-  noOfSquares++;
+  container.className = 'container';
+
+  container.style.width =  `${containerWidth+colGaps}px`;
+  container.style.height = `${containerHeight+rowGaps}px`;
+
+  let noOfSquares = 1;
+  while (noOfSquares < totalSquares) {
+    const squareCopy = square.cloneNode(true);
+    container.appendChild(squareCopy);
+    noOfSquares++;
+  }
 }
+
+displayGrid(noOfRows, noOfCols);
