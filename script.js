bc711e3ff2c58a638ca4ae80d26b06c1004a4637
Otoolorin <volasupo5@gmail.com>
2025-02-14 10:26:45 +0100

    Populate the grid with all total squares
    I moved the "square" div reference to the loop so
    as to create a new square for every time a square is
    to be added to the grid.

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ const totalSqrs = 16**2;
 function displayGrid(totalSqrs) {
 
   const grid = document.querySelector('.container');
-  const square = document.createElement('div');
 
   const gridWidth = grid.offsetWidth;
   const squareSize = gridWidth / totalSqrs;
 
+  let noOfSquares = 0;
+  while (noOfSquares < totalSqrs) {
+    const square = document.createElement('div');
+    grid.appendChild(square);
+    noOfSquares++;
+  }
   console.log(squareSize); 
 }
