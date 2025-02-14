426ed337748051e8422c9a46b5920eac3b697e07
Otoolorin <volasupo5@gmail.com>
2025-02-14 10:04:05 +0100

    Store references of the grid and a square

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const noOfSqrs = 16;
 const totalSqrs = 16**2;
 
 function displayGrid(totalSqrs) {
-  
+
+  const grid = document.querySelector('.container');
+  const square = document.createElement('.div');
+
 }
